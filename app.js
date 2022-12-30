a5c5f7b7b9b261546e682a1642a8eabe3f8d0df9
ZiuChen <457353192@qq.com>
2022-12-30 16:20:08 +0800

    feat: 支持基础的JS-SDK调用

--- app.js
+++ app.js
@@ -2,13 +2,44 @@ const express = require('express')
 const auth = require('./src/wechat/auth')
 const app = express()
 
+const Wechat = require('./src/wechat/wechat')
+const { appID, url } = require('./src/config')
+const sha1 = require('sha1')
+
 // 配置模板资源目录
 app.set('views', './src/views')
 // 配置模板引擎
 app.set('view engine', 'ejs')
 // 页面路由
-app.get('/search', (req, res) => {
-  res.render('search')
+app.get('/search', async (req, res) => {
+  /*
+    生成js-sdk使用的签名
+      1. 组合四个参数 jsapi_ticket noncestr timestamp url
+      2. 字典序排序 '&'拼接
+      3. sha1签名 生成signature
+   */
+  const w = new Wechat()
+  const { ticket } = await w.getTicket()
+  const noncestr = Math.random().toString().split('.')[1]
+  const timestamp = Date.now()
+
+  const str = [
+    `jsapi_ticket=${ticket}`,
+    `noncestr=${noncestr}`,
+    `timestamp=${timestamp}`,
+    `url=${url}/search`
+  ]
+    .sort()
+    .join('&')
+
+  const signature = sha1(str)
+
+  res.render('search', {
+    appID,
+    signature,
+    noncestr,
+    timestamp
+  })
 })
 
 app.use(auth())
--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,6 @@ require('dotenv').config()
 module.exports = {
   token: process.env.token,
   appID: process.env.TENCENTCLOUD_RUNENV ? process.env.appID : process.env.DEV_appID,
-  appsecret: process.env.TENCENTCLOUD_RUNENV ? process.env.appsecret : process.env.DEV_appsecret
+  appsecret: process.env.TENCENTCLOUD_RUNENV ? process.env.appsecret : process.env.DEV_appsecret,
+  url: process.env.TENCENTCLOUD_RUNENV ? process.env.url : process.env.DEV_url
 }
